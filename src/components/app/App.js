92efa89f142f57bd99a590233d17b4ed7dcc998e
Artem <tema25062010@gmail.com>
2026-04-18 13:02:18 +0300

    make second block 'about'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import '../../css/modern-normalize.min.css';
 import Header from '../header/Header';
 import Welcome from '../welcome/Welcome';
+import About from '../about/About';
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Welcome/>
+            <About/>
         </div>
     )
 }
