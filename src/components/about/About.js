0c11aada147bd305f1d83767c707bcbb7bd42e23
Artem <tema25062010@gmail.com>
2026-04-19 14:27:33 +0300

    slider for about block

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,22 +6,40 @@ import sliderImg3 from '../../images/about/AboutImg3.png';
 import sliderImg4 from '../../images/about/AboutImg4.png';
 import sliderImg5 from '../../images/about/AboutImg5.png';
 
+import { useState } from 'react';
+
 const About = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const [offset, setOffset] = useState(0);
+
+
+    const onChange = (index) => {
+        setSlideIndex(index);
+        setOffset(551 * index);
+    };
+
+    const styles = {
+        transform: `translateX(-${offset}px)`,
+        transition: `all 0.6s ease-in-out`
+    };
+
     return (
         <div className="about">
             <h2 className="about__title">About</h2>
             <p className="about__description">The Brooklyn Library is a free workspace, a large number of books and a cozy coffee shop inside </p>
-            <div className="about__images">
-                <img src={sliderImg1} alt="slider image" className="about__img" />
-                <img src={sliderImg2} alt="slider image" className="about__img" />
-                <img src={sliderImg3} alt="slider image" className="about__img" />
-                <img src={sliderImg4} alt="slider image" className="about__img" />
-                <img src={sliderImg5} alt="slider image" className="about__img" />
+            <div className="about__images-wrapper">
+                <div style={styles} className="about__images">
+                    <img src={sliderImg1} alt="slider image" className="about__img" />
+                    <img src={sliderImg2} alt="slider image" className="about__img" />
+                    <img src={sliderImg3} alt="slider image" className="about__img" />
+                    <img src={sliderImg4} alt="slider image" className="about__img" />
+                    <img src={sliderImg5} alt="slider image" className="about__img" />
+                </div>
             </div>
             <div className="about__pagination">
-                <div id="active" className="pagination__point pagination__point-active"></div>
-                <div className="pagination__point"></div>
-                <div className="pagination__point"></div>
+                <div onClick={() => onChange(0)} id="active" className="pagination__point pagination__point-active"></div>
+                <div onClick={() => onChange(1)} className="pagination__point"></div>
+                <div onClick={() => onChange(2)} className="pagination__point"></div>
             </div>
         </div>
     )
