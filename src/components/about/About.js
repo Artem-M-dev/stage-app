4e240d55d90c151ec69092fbd2c2f2003e372ad2
Artem <tema25062010@gmail.com>
2026-04-19 23:28:05 +0300

    make slider in About component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,22 +9,56 @@ import sliderImg5 from '../../images/about/AboutImg5.png';
 import { Component } from 'react';
 
 class About extends Component {
+
+    state = {
+        index: 0,
+        offset: 0
+    };
+
+    onChange = (index) => {
+        this.setState({
+            index,
+            offset: 551 * index
+        })
+    };
+
     render() {
+        const {offset} = this.state;
+
+        const styles = {
+            transform: `translateX(-${offset}px)`,
+            transition: `all 0.6s ease-in-out`
+        };
+
         return (
             <div className="about">
                 <h2 className="about__title">About</h2>
                 <p className="about__description">The Brooklyn Library is a free workspace, a large number of books and a cozy coffee shop inside </p>
-                <div className="about__images">
-                    <img src={sliderImg1} alt="slider image" className="about__img" />
-                    <img src={sliderImg2} alt="slider image" className="about__img" />
-                    <img src={sliderImg3} alt="slider image" className="about__img" />
-                    <img src={sliderImg4} alt="slider image" className="about__img" />
-                    <img src={sliderImg5} alt="slider image" className="about__img" />
+                <div className="about__images-wrapper">
+                    <div style={styles} className="about__images">
+                        <img src={sliderImg1} alt="slider image" className="about__img" />
+                        <img src={sliderImg2} alt="slider image" className="about__img" />
+                        <img src={sliderImg3} alt="slider image" className="about__img" />
+                        <img src={sliderImg4} alt="slider image" className="about__img" />
+                        <img src={sliderImg5} alt="slider image" className="about__img" />
+                    </div>
                 </div>
                 <div className="about__pagination">
-                    <div id="active" className="pagination__point pagination__point-active"></div>
-                    <div className="pagination__point"></div>
-                    <div className="pagination__point"></div>
+                    <div 
+                        onClick={() => this.onChange(0)}
+                        id="0"
+                        className={`pagination__point ${this.state.index === 0 ? 'pagination__point-active' : null}`}
+                        ></div>
+                    <div 
+                        onClick={() => this.onChange(1)}
+                        id="1"
+                        className={`pagination__point ${this.state.index === 1 ? 'pagination__point-active' : null}`}
+                        ></div>
+                    <div 
+                        onClick={() => this.onChange(2)}
+                        id="2"
+                        className={`pagination__point ${this.state.index === 2 ? 'pagination__point-active' : null}`}
+                        ></div>
                 </div>
             </div>
         )
