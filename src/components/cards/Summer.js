bcf3a77002a27552f212b31bd26676e7b3cad765
Artem <tema25062010@gmail.com>
2026-04-26 12:00:20 +0300

    Add new folder with images for 'favorites' cards into public folder. Also creating cards based on data from the database. For functional components

--- src/components/cards/Summer.js
+++ src/components/cards/Summer.js
@@ -1,39 +1,38 @@
-import summerBook1 from '../../images/favorites/FavoritesSummerBook1.png';
-import summerBook2 from '../../images/favorites/FavoritesSummerBook2.png';
-import summerBook3 from '../../images/favorites/FavoritesSummerBook3.png';
-import summerBook4 from '../../images/favorites/FavoritesSummerBook4.png';
+import {useState, useEffect} from 'react'
 
 const Summer = (props) => {
+
+    const [cards, setCards] = useState([]);
+    
+    useEffect(() => {
+        fetchElements();
+    }, [])
+
+    const fetchElements = async () => {
+        try {
+            const res = await fetch('http://localhost:3001/books')
+            const data = await res.json();
+            
+            setCards(data[0].summer)
+            
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
     return (
-        <div id="summer" className="favorites__books">
-            <div className="favorites__book">
-                <p className="favorites__book-type">Staff Picks</p>
-                <h3 className="favorites__book-title">Crude: A Memoir <br />By Pablo Fajardo & Sophie Tardy-Joubert</h3>
-                <p className="favorites__book-description">Drawing and color by Damien Roudeau | This book illustrates the struggles of a group of indigenous Ecuadoreans as they try to sue the ChevronTexaco company for damage their oil fields did to the Amazon and her people</p>
-                <button className="favorites__book-buy">Buy</button>
-                <img src={summerBook1} alt="book" className="favorites__book-image" />
-            </div>
-            <div className="favorites__book">
-                <p className="favorites__book-type">Staff Picks</p>
-                <h3 className="favorites__book-title">Let My People Go Surfing <br />By Yvon Chouinard</h3>
-                <p className="favorites__book-description">Chouinard—climber, businessman, environmentalist—shares tales of courage and persistence from his experience of founding and leading Patagonia, Inc. Full title: Let My People Go Surfing: The Education of a Reluctant Businessman, Including 10 More Years of Business Unusual.</p>
-                <button className="favorites__book-buy">Buy</button>
-                <img src={summerBook2} alt="book" className="favorites__book-image" />
-            </div>
-            <div className="favorites__book">
-                <p className="favorites__book-type">Staff Picks</p>
-                <h3 className="favorites__book-title">The Octopus Museum: Poems <br />By Brenda Shaughnessy</h3>
-                <p className="favorites__book-description">This collection of bold and scathingly beautiful feminist poems imagines what comes after our current age of environmental destruction, racism, sexism, and divisive politics.</p>
-                <button className="favorites__book-buy">Buy</button>
-                <img src={summerBook3} alt="book" className="favorites__book-image" />
-            </div>
-            <div className="favorites__book">
-                <p className="favorites__book-type">Staff Picks</p>
-                <h3 className="favorites__book-title">Shark Dialogues: A Novel <br />By Kiana Davenport</h3>
-                <p className="favorites__book-description">An epic saga of seven generations of one family encompasses the tumultuous history of Hawaii as a Hawaiian woman gathers her four granddaughters together in an erotic tale of villains and dreamers, queens and revolutionaries, lepers and healers.</p>
-                <button className="favorites__book-buy">Buy</button>
-                <img src={summerBook4} alt="book" className="favorites__book-image" />
-            </div>
+        <div id="winter" className="favorites__books">
+            {
+                cards.map(item => (
+                    <div key={item.id} className="favorites__book">
+                        <p className="favorites__book-type">{item.type}</p>
+                        <h3 className="favorites__book-title">{item.title}<br />{item.author}</h3>
+                        <p className="favorites__book-description">{item.description}</p>
+                        <button className="favorites__book-buy">Buy</button>
+                        <img className="favorites__book-image" src={item.image} alt="book" />
+                    </div>
+                ))
+            }
         </div>
     )
 }
